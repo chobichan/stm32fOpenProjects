ecbb3545ff649f66d50d2bcc590e57f4a585e446
chobichan <hamayan.contact@gmail.com>
2020-04-10 15:21:19 +0900

    add licence.

--- myOs/mul_tsk.c
+++ myOs/mul_tsk.c
@@ -1,9 +1,26 @@
-/****************************************************************************/
-/* マルチタスク実行                                                         */
-/*                                                                          */
-/* designed by hamayan                                                      */
-/*                         Copyright (C) 2012 hamayan All Rights Reserved.  */
-/****************************************************************************/
+/* ----------------------------------------
+ not preempt multi task service call routines.
+
+  Copyright (c) 2020 hamayan (hamayan.contact@gmail.com).
+  All right reserved.
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+  Created 2020 by hamayan (hamayan.contact@gmail.com)
+---------------------------------------- */
+
 #include "mul_tsk.h"  //必要
 
 /****************************************************************************/
